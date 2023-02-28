c6052db5bc33993cf001e5c9fb2cccad88bdbbdd
Stypek123 <mati6969s@gmail.com>
2023-02-28 19:03:50 +0100

    contac working

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ContactPage = () => {
-  return (
-    <div>
-      <p>kontakt</p>
-    </div>
-  );
-};
+import '../styles/ContactPage.css';
+
+class ContactPage extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      value: '',
+    });
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            placeholder="Napisz swoją wiadomość"
+            value={this.state.value}
+            onChange={this.handleChange}></textarea>
+          <button>Wyślij</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
