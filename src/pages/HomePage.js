38d0548cac67470c0baf270bfad19bd0b7fbbaf9
Stypek123 <mati6969s@gmail.com>
2023-02-28 17:23:46 +0100

    home working with articles, import font fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import Article from '../components/Article';
+
+const articles = [
+  {
+    id: 1,
+    title: 'Czym jest teoria strun?',
+    author: 'Jan Nowak',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque amet, vitae consequatur exercitationem ratione voluptatibus repudiandae alias deleniti culpa illo suscipit quia sint temporibus illum commodi itaque ea ab ipsum?',
+  },
+  {
+    id: 2,
+    title: 'Czym jest paradoks fermiego?',
+    author: 'Ania Kwiatkowska',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cumque accusantium sapiente perspiciatis id ipsam necessitatibus recusandae unde dolores error sunt officiis ad, voluptatum veniam, blanditiis nam in totam esse.',
+  },
+  {
+    id: 3,
+    title: 'Ciemna materia i ciemna energia?',
+    author: 'Jan Kowalski',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cumque accusantium sapiente perspiciatis id ipsam necessitatibus recusandae unde dolores error sunt officiis ad, voluptatum veniam, blanditiis nam in totam esse.',
+  },
+];
+
 const HomePage = () => {
-  return (
-    <div>
-      <p>Home</p>
-    </div>
-  );
+  const artList = articles.map((article) => <Article key={article.id} article={article} />);
+
+  return <div className="home">{artList}</div>;
 };
 
 export default HomePage;
