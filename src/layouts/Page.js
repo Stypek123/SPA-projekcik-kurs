11b882872a0ead5ab20934f7c418855f3c1d265a
Stypek123 <mati6969s@gmail.com>
2023-02-28 17:05:26 +0100

    working page, pages added

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import ProductPage from '../pages/ProductPage';
+import ContactPage from '../pages/ContactPage';
+import AdminPage from '../pages/AdminPage';
+import ErrorPage from '../pages/ErrorPage';
 
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/*" element={<ErrorPage />} />
+    </Routes>
+  );
 };
 
 export default Page;
---
+++ src/pages/AdminPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminPage = () => {
+  return (
+    <div>
+      <p>admin</p>
+    </div>
+  );
+};
+
+export default AdminPage;
