5a888c0fadfb806f4f5773215f146800f39592b0
Stypek123 <mati6969s@gmail.com>
2023-03-02 16:04:53 +0100

    update

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   state = {};
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="app">
           <header>{<Header />}</header>
 
